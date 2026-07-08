1eff5461fe1c52575551b96cb241b9a70a9be25e
Awwalu <awwalu253@gmail.com>
2026-07-08 20:44:30 +0100

    Add brand logo to navbar and footer

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function loadNavbar() {
     navbar.innerHTML = `
         <div class="container">
             <a href="index.html">
-                <img src="/logo.png" alt="Awwalu Kitchen Vault" class="logo" />
+                <img src="https://res.cloudinary.com/dszfpg8hj/image/upload/v1783539694/logo_m1tvqv.png" class="logo" />
             </a>
             <div class="nav-links">
                 <a href="index.html" class="${window.location.pathname.includes('index') ? 'active' : ''}">Home</a>
@@ -46,7 +46,7 @@ function loadFooter() {
     if (!footer) return;
     footer.innerHTML = `
         <div class="container">
-            <img src="/logo.png" alt="Awwalu Kitchen Vault" style="height:48px; margin-bottom:12px;" />
+            <img src="https://res.cloudinary.com/dszfpg8hj/image/upload/v1783539694/logo_m1tvqv.png" />
             <p>&copy; 2026 Awwalu Kitchen Vault. All rights reserved.</p>
             <p class="small">Premium kitchen appliances, cookware, and utensils.</p>
         </div>
